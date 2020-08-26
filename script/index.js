2ecb6ee5406a1e6f59574251308fb6bf278311b7
Carlos Taracena <charlietaracenadrums@gmail.com>
2020-08-26 17:32:53 -0500

    bloqueo en hora de clases y limpiar el input al dar click

--- script/index.js
+++ script/index.js
@@ -4,11 +4,22 @@ const msg = document.querySelector('.msg').valueOf();
 const codificado = document.querySelector('.codificado');
 const reload = document.querySelector('.reload');
 
+const block = document.querySelector('.block');
+
+function blockLayer() {
+    block.classList = 'layer';
+} 
+
+let date = new Date();
+let hour = date.getHours();
+
+
 convert.addEventListener('click', () => {
+    if (hour > 8 && hour < 14) {
+        block.classList = 'layer';
+    }
     let message = msg.value.toLowerCase();
-    console.log(message);
     let msgArr = Array.from(message)
-    console.log(msgArr);
     let convertedArr = []
     for (i = 0; i < msgArr.length; i++) {
         switch (msgArr[i]) {
@@ -96,19 +107,65 @@ convert.addEventListener('click', () => {
             case 'z':
                 convertedArr.push('-');
             break
+            case '0':
+                convertedArr.push('q');
+            break
+            case '1':
+                convertedArr.push('w');
+            break
+            case '2':
+                convertedArr.push('e');
+            break
+            case '3':
+                convertedArr.push('r');
+            break
+            case '4':
+                convertedArr.push('t');
+            break
+            case '5':
+                convertedArr.push('y');
+            break
+            case '6':
+                convertedArr.push('u');
+            break
+            case '7':
+                convertedArr.push('i');
+            break
+            case '8':
+                convertedArr.push('o');
+            break
+            case '9':
+                convertedArr.push('p');
+            break
+            case 'á':
+                convertedArr.push('[');
+            break
+            case 'é':
+                convertedArr.push(']');
+            break
+            case 'í':
+                convertedArr.push('|');
+            break
+            case 'ó':
+                convertedArr.push('@');
+            break
+            case 'ú':
+                convertedArr.push('#');
+            break
         }
         
     }
-    console.log(convertedArr);
     let coded = convertedArr.join('');
     codificado.innerHTML = coded;
+    msg.value = '';
 })
 
 decodificar.addEventListener('click', () => {
+    if (hour > 8 && hour < 14) {
+        block.classList = 'layer';
+    }
     let message = msg.value;
-    console.log(message);
     let msgArr = Array.from(message)
-    console.log(msgArr);
     let convertedArr = []
     for (i = 0; i < msgArr.length; i++) {
         switch (msgArr[i]) {
@@ -196,10 +253,56 @@ decodificar.addEventListener('click', () => {
             case '-':
                 convertedArr.push('z');
             break
+            case 'q':
+                convertedArr.push('0');
+            break
+            case 'w':
+                convertedArr.push('1');
+            break
+            case 'e':
+                convertedArr.push('2');
+            break
+            case 'r':
+                convertedArr.push('3');
+            break
+            case 't':
+                convertedArr.push('4');
+            break
+            case 'y':
+                convertedArr.push('5');
+            break
+            case 'u':
+                convertedArr.push('6');
+            break
+            case 'i':
+                convertedArr.push('7');
+            break
+            case 'o':
+                convertedArr.push('8');
+            break
+            case 'p':
+                convertedArr.push('9');
+            break
+            case '[':
+                convertedArr.push('á');
+            break
+            case ']':
+                convertedArr.push('é');
+            break
+            case '|':
+                convertedArr.push('í');
+            break
+            case '@':
+                convertedArr.push('ó');
+            break
+            case '#':
+                convertedArr.push('ú');
+            break
         }
     }
     let stringCode = convertedArr.join('');
     codificado.innerHTML = stringCode;
+    msg.value = '';
 })
 
 reload.addEventListener('click', () => {
